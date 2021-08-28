7c200bc39fe4ccb6630902b2e5201ee9d7cbc04b
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2021-08-27 23:28:36 -0500

    EF Fundamentals Initial

---
+++ EFFundamentals/EFFundamentals/Models/DeptEmpLatestDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFFundamentals.Models
+{
+    public partial class DeptEmpLatestDate
+    {
+        public int EmpNo { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
---
+++ EFFundamentals/EFFundamentals/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using EFFundamentals.Models;
+
+namespace EFFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating context
+            // the context may be instanced as any other object in the language
+            var context = new employeesContext ();
+
+            // **************************** read *******************************
+            // through context we can iterate over dbset (entities) exposed 
+            /*foreach (var row in context.Departments)
+            {
+                Console.WriteLine(row.DeptName);
+            }*/
+
+            // **************************** save *******************************
+            // to append a new element into a dbset utilize add method passing an object
+            // related to dbset inner type
+            /*context.Departments.Add (
+
+                new Department
+                {
+                     DeptNo = "1111",
+                     DeptName = "VOLKSWAGEN"
+                }
+
+            );*/
+
+            // EF remains all changes in memory and towards persist them in real DB
+            // saveChanges method must be called
+            //context.SaveChanges ();
+
+            // **************************** search *******************************
+            // DBSet expose some methods that permit perform some operations over entities,
+            // Find method receive an array of primary keys formatted as object array and returns
+            // an entity related with that keys
+            //var department = context.Departments.Find (new object [] { "1111" });
+            //if (department != null)
+                //Console.WriteLine (department.DeptName);
+
+            // **************************** delete *******************************
+            // Other recurrent action performed with entities is delete it, to do so
+            // DbSet count with Remove method, this method receive as parameter the entity that
+            // want be deleted
+            var department = context.Departments.Find (new object [] { "1111" });
+            if (department != null)
+                context.Departments.Remove (department);
+
+            Console.ReadKey ();
+        }
+    }
+}
